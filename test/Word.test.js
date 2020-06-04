32b3a108ad3d22ee4ed0290c07d784f5ad39080d
ichoi21 <ihchoi@gmail.com>
2020-06-03 19:39:29 -0700

    updated with set function-ish

--- test/Word.test.js
+++ test/Word.test.js
@@ -6,19 +6,19 @@ describe("Word", () => {
       const testWord = new Word("test");
       expect(testWord).toEqual({ word: "test" });
     });
-
-    describe("getWord", () => {
-      it("Should return word", () => {
-        const testWord = new Word("test");
-        expect(testWord.getWord).toBe("test");
-      });
-
-
-    describe("setWord", () => {
-      it("Should provide word", () => {
-        const testWord = new Word("test");
-        expect(testWord.setWord()).toBe("test");
-      });
+  });
+  describe("getWord", () => {
+    it("Should return word", () => {
+      const testWord = new Word("test");
+      expect(testWord.getWord).toBe("test");
+    });
+  });
+  describe("setWord", () => {
+    it("Should provide word that was passed", () => {
+      const testWord = new Word("test");
+      testWord.setWord = "thing";
+      expect(testWord).toEqual({ word: "thing" });
     });
   });
 });
+//you want to write the code to pass the test not test to pass the code.
